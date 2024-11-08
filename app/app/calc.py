f0eb2cdd008faa73ad76c702a1351d22f1eb48a1
AcaZivic <acazivic575@gmail.com>
2024-11-09 00:33:31 +0100

    last-fix-v.5

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """ADD x and y and return result."""
+    return x+y;
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTestCase(SimpleTestCase):
+    """Test the calc function."""
+    def test_add_numbers(self):
+        """Testing adding numbers."""
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
