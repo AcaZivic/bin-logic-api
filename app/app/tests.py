6dca38ec53d3d7304e433e20e4815ffa6fd6b4af
AcaZivic <acazivic575@gmail.com>
2024-12-07 00:53:36 +0100

    commit wait_for_db

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,12 +5,10 @@ Sample tests
 from django.test import SimpleTestCase
 
 from app import calc
-
-
 class CalcTestCase(SimpleTestCase):
     """Test the calc function."""
     def test_add_numbers(self):
         """Testing adding numbers."""
-        res = calc.add(5, 6)
+        res = calc.add(5, 11)
 
         self.assertEqual(res, 11)
